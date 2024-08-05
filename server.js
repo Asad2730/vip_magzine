c183d4ad2be094e063098e31fae0520dbd61fb43
Asad2730 <asadsajjad777@gmail.com>
2024-08-05 18:37:21 +0500

    ...

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-
-fs.readFile('./login.html', (err, html) => {
-    if (err) {
-        throw err;
-    }
-
-
-    http.createServer((req, res) => {
+// Function to serve HTML files
+function serveFile(res, filePath) {
+    fs.readFile(filePath, (err, html) => {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Server Error');
+            return;
+        }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(html);
-    }).listen(3000, () => {
-        console.log('Server running at http://localhost:3000/');
     });
+}
+
+http.createServer((req, res) => {
+    switch (req.url) {
+        case '/signup':
+            serveFile(res, path.join(__dirname, 'signup.html'));
+            break;
+        case '/forgetPassword':
+            serveFile(res, path.join(__dirname, 'forgetPassword.html'));
+            break;
+        default:
+            serveFile(res, path.join(__dirname, 'login.html'));
+            break;
+    }
+}).listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
 });
